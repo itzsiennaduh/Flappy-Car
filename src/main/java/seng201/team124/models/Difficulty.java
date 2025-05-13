0d87357f921acfb1a06081fd9a15a3b9f33ee025
sienna <sro179@uclive.ac.nz>
2025-05-13 21:43:30 +1200

    added checkstyle id to build.gradle for use at the end (checks documentations). created the difficulty enum to be implemented with the slider in the start menu, values can be finalised at the end (game logic very incomplete haha). player class updated.

---
+++ src/main/java/seng201/team124/models/Difficulty.java
@@ -0,0 +1,59 @@
+package seng201.team124.models;
+
+/**
+ * Enum for difficulty levels
+ */
+public enum Difficulty {
+    EASY(1.5, 0.7, 0.5),
+    MEDIUM(1.0, 1.0, 1.0),
+    HARD(0.5, 1.3, 1.5);
+
+    private final double moneyMultiplier; //prize money
+    private final double eventChanceMultiplier;
+    private final double routeDifficultyMultiplier;
+
+    /**
+     * constructs a difficulty level with the specified multipliers
+     * @param moneyMultiplier multiplier for prize money
+     * @param eventChanceMultiplier multiplier for event chances
+     * @param routeDifficultyMultiplier multiplier for route difficulty
+     */
+    Difficulty(double moneyMultiplier, double eventChanceMultiplier, double routeDifficultyMultiplier) {
+        this.moneyMultiplier = moneyMultiplier;
+        this.eventChanceMultiplier = eventChanceMultiplier;
+        this.routeDifficultyMultiplier = routeDifficultyMultiplier;
+    }
+
+    /**
+     * gets the money multiplier for this difficulty level
+     * @return money multiplier
+     */
+    public double getMoneyMultiplier() {
+        return moneyMultiplier;
+    }
+
+    /**
+     * gets the random event chance multiplier for this difficulty level
+     * @return multiplier for event chances
+     */
+    public double getEventChanceMultiplier() {
+        return eventChanceMultiplier;
+    }
+
+    /**
+     * gets the route difficulty multiplier for this difficulty level
+     * @return multiplier for route difficulty
+     */
+    public double getRouteDifficultyMultiplier() {
+        return routeDifficultyMultiplier;
+    }
+
+    /**
+     * calculates the starting money for a player based on the difficulty level
+     * @param baseMoney Base money amount
+     * @return starting money adjusted for difficulty
+     */
+    public int calculateStartingMoney(int baseMoney) {
+        return (int) (baseMoney * getMoneyMultiplier());
+    }
+}
--- src/main/java/seng201/team124/models/Player.java
+++ src/main/java/seng201/team124/models/Player.java
@@ -1,4 +1,208 @@
 package seng201.team124.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * represents all the data belonging to the player
+ */
 public class Player {
+    private String name; //players name between 3-15 char incl
+    private int money; //money im dollars accessible to the player
+    private final List<Vehicle> vehicles; //list of vehicles owned by the player
+    private final List<TuningParts> tuningParts; //list of tuning parts owned by the player (not yet installed)
+    private Vehicle currentVehicle; //the vehicle the player is currently driving
+
+    /**
+     * constructor for the player with their set name and initial money based on difficulty
+     * @param name the name of the player, between 3-15 characters long
+     * @param money the initial money of the player based on difficulty setting
+     */
+    public Player(String name, int money) {
+        this.name = name;
+        this.money = money;
+        this.vehicles = new ArrayList<>();
+        this.tuningParts = new ArrayList<>();
+        this.currentVehicle = null;
+    }
+
+    /**
+     * gets the name of the player
+     * @return the name of the player as a string, between 3-15 characters long.
+     */
+    public String getName() {
+        return this.name;
+    }
+
+    /**
+     * gets the current money of the player
+     * @return current money amount
+     */
+    public int getMoney() {
+        return this.money;
+    }
+
+    /**
+     * adds money to the player's account
+     * @param amount the amount of money to add
+     */
+    public void addMoney(int amount) {
+        this.money += amount;
+    }
+
+    /**
+     * subtracts money from the player's account
+     * @param amount the amount of money to subtract
+     * @return true if the player has enough money to subtract the amount, false otherwise
+     */
+    public boolean subtractMoney(int amount) {
+        if (amount > this.money) {
+            return false;
+        }
+        this.money -= amount;
+        return true;
+    }
+
+    /**
+     * gets the list of vehicles owned by the player
+     * @return list of vehicles owned by the player
+     */
+    public List<Vehicle> getVehicles() {
+        return new ArrayList<>(vehicles); //copy
+    }
+
+    /**
+     * adds a vehicle to the list of vehicles owned by the player
+     * @param vehicle the vehicle to add
+     * @return true if successful, false if max capacity
+     * @throws IllegalStateException if the player already owns the vehicle
+     */
+    public boolean addVehicle(Vehicle vehicle) {
+        if (vehicles.contains(vehicle)) {
+            throw new IllegalStateException(String.format("%s already owned.", vehicle.getName()));
+        }
+        if (vehicles.size() >= 5) {
+            return false;
+        }
+        vehicles.add(vehicle);
+
+        if (currentVehicle == null) {
+            currentVehicle = vehicle;
+        }
+        return true;
+    }
+
+    /**
+     * removes a vehicle from the list of vehicles owned by the player (sold the car)
+     * @param vehicle the vehicle to remove
+     * @return true if successful, false if was the player's last vehicle
+     * @throws IllegalStateException if the player does not own the vehicle
+     */
+    public boolean removeVehicle(Vehicle vehicle) {
+        if (!vehicles.contains(vehicle)) {
+            throw new IllegalStateException(String.format("%s is not owned, so can't be removed.", vehicle.getName()));
+        }
+        if (vehicles.size() <= 1) {
+            return false;
+        }
+        boolean result = vehicles.remove(vehicle);
+
+        if (result && currentVehicle == vehicle) {
+            currentVehicle = vehicles.getFirst();
+        }
+        return result;
+    }
+
+    /**
+     * gets the currently selected vehicle
+     * @return the currently selected vehicle
+     */
+    public Vehicle getCurrentVehicle() {
+        return currentVehicle;
+    }
+
+    /**
+     * sets the currently selected vehicle
+     * @param vehicle the vehicle to set as the current vehicle
+     * @return true if successful, false if the player does not own the vehicle
+     */
+    public boolean setCurrentVehicle(Vehicle vehicle) {
+        if (!vehicles.contains(vehicle)) {
+            return false;
+        }
+        currentVehicle = vehicle;
+        return true;
+    }
+
+    /**
+     * gets all uninstalled tuning parts owned by the player
+     * @return list of tuning parts owned by the player
+     */
+    public List<TuningParts> getTuningParts() {
+        return new ArrayList<>(tuningParts); //copy
+    }
+
+    /**
+     * adds a tuning part to the list of tuning parts owned by the player (inventory)
+     * @param part the tuning part to add
+     */
+    public void addTuningPart(TuningParts part) {
+        tuningParts.add(part);
+    }
+
+    /**
+     * removes a tuning part from the list of tuning parts owned by the player (inventory)
+     * @param part the tuning part to remove
+     * @return true if successful
+     * @throws IllegalStateException if the player does not own the tuning part
+     */
+    public boolean removeTuningPart(TuningParts part) {
+        if (!tuningParts.contains(part)) {
+            throw new IllegalStateException(String.format("%s is not owned, so can't be removed.", part.getName()));
+        }
+        return tuningParts.remove(part);
+    }
+
+    /**
+     * installs a tuning part on the current vehicle
+     * @param part the tuning part to install
+     * @param vehicle the vehicle to install the tuning part on
+     * @return installation message or error message
+     */
+    public String installTuningPart(TuningParts part, Vehicle vehicle) {
+        if (!vehicles.contains(vehicle)) {
+            return String.format("%s is not owned, so %s can't be installed.", vehicle.getName(), part.getName());
+        }
+        if (!tuningParts.contains(part)) {
+            return String.format("%s is not owned, so it can't be installed on %s.", part.getName(), vehicle.getName());
+        }
+
+        try {
+            vehicle.installPart(part);
+            tuningParts.remove(part);
+            return String.format("%s successfully installed on %s!", part.getName(), vehicle.getName());
+        } catch (IllegalStateException e) {
+            return e.getMessage();
+        }
+    }
+
+    /**
+     * uninstalls a tuning part from the current vehicle
+     * @param part the tuning part to uninstall
+     * @param vehicle the vehicle to uninstall the tuning part from
+     * @return uninstallation message or error message
+     */
+    public String uninstallTuningPart(TuningParts part, Vehicle vehicle) {
+        if (!vehicles.contains(vehicle)) {
+            return String.format("%s is not owned, so %s can't be uninstalled.", vehicle.getName(), part.getName());
+        }
+
+        try {
+            vehicle.removePart(part);
+            tuningParts.add(part);
+            return String.format("%s successfully uninstalled from %s!", part.getName(), vehicle.getName());
+        } catch (IllegalStateException e) {
+            return e.getMessage();
+        }
+    }
 }
